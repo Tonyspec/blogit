31fc1a21036a3f022775a80b39bfbd61b5699d89
Anton Achedžak <achedant@student.cvut.cz>
2025-01-08 21:19:14 +0100

    ready for cat

--- myblogsite/blog/views.py
+++ myblogsite/blog/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render, get_object_or_404
 from .models import Post, ProfileUser, Like, Comment
 from django.shortcuts import render, redirect
 from django.http import JsonResponse
-from .forms import CommentForm, UserSignUpForm, LoginForm 
+from .forms import CommentForm, UserSignUpForm, LoginForm, ProfileEditForm
 
 from django.contrib.auth import login
 
@@ -44,7 +44,22 @@ def post_detail(request, pk):
         'form': form,
     })
 
+from django.template.loader import render_to_string
+from django.utils.html import strip_tags
+from django.core.mail import EmailMultiAlternatives
+from django.conf import settings
 
+
+def send_registration_email(user_name, user_email):
+    subject = 'Welcome to MyWeb'
+    html_content = render_to_string('welcome_email.html', {'user_name': user_name})
+    text_content = strip_tags(html_content)  # Plain text version for email clients without HTML support
+
+    email = EmailMultiAlternatives(
+        subject, text_content, settings.EMAIL_HOST_USER, [user_email]
+    )
+    email.attach_alternative(html_content, "text/html")
+    email.send(fail_silently=False, verify=False)
 
 def signup_view(request):
     if request.method == 'POST':
@@ -53,6 +68,7 @@ def signup_view(request):
             user = form.save()
             # Log the user in
             login(request, user)
+            send_registration_email(user.name, user.email)  
             messages.success(request, 'Your account has been created successfully!')
             return redirect('home')  # Redirect to a success page or home page
     else:
@@ -80,17 +96,7 @@ def custom_login(request):
 
 
 
-@login_required
-def edit_profile(request):
-    if request.method == 'POST':
-        form = ProfileEditForm(request.POST, request.FILES, instance=request.user)
-        if form.is_valid():
-            form.save()
-            messages.success(request, 'Profile edited successfully!')
-            return redirect('profile')  # Or another appropriate redirection
-    else:
-        form = ProfileEditForm(instance=request.user)
-    return render(request, 'edit_profile.html', {'form': form})
+
 
 @login_required(login_url='login')
 def profile(request, username=None):
@@ -104,8 +110,26 @@ def profile(request, username=None):
     
     return render(request, 'profile.html', {
         'user_profile': user,
-        'posts': posts
+        'posts': posts,
+        'username': username
     })
+
+@login_required
+def edit_profile(request, username):
+    user = get_object_or_404(ProfileUser, username=username)
+    if user != request.user:
+        # You might want to redirect or raise an error if someone tries to edit another user's profile
+        messages.error(request, "You can only edit your own profile.")
+        return redirect('profile')  # Assuming 'profile' is a URL name for the user's profile
+    if request.method == 'POST':
+        form = ProfileEditForm(request.POST, request.FILES, instance=user)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Profile edited successfully!')
+            return redirect('profile', username=username)  # Or another appropriate redirection
+    else:
+        form = ProfileEditForm(instance=request.user)
+    return render(request, 'edit_profile.html', {'form': form})
 
 #CREATE POST
 @login_required
--- myblogsite/myblogsite/settings.py
+++ myblogsite/myblogsite/settings.py
@@ -143,3 +143,12 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 LOGIN_URL = '/accounts/login/'
 LOGIN_REDIRECT_URL = 'home'
+
+
+#EMAILS
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.mail.me.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = 'news_nest@icloud.com'
+EMAIL_HOST_PASSWORD = 'pzbj-ovxw-mtxs-jrmq'  # Note: Not your regular password but an app-specific password
